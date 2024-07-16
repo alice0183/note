0b14c2cdb92cdbf1dc08b129e91f07c95ad73c6d
Q <qwe246858@gmail.com>
2024-07-16 23:03:19 +0800

    找關鍵字

--- homework.py
+++ homework.py
@@ -1,8 +1,10 @@
-s1='https://www.youtube.com/results?search_query=LiSA-ADAMAS'
-s2=(s1[45:])
-print(s2.replace('+',' '))
+s1=('https://www.youtube.com/results?search_query=LiSA-ADAMAS'.partition('='))
+#先將字串分割成三段，以=為區分標準
+print(s1[2].replace('+',' '))       
+#取字串中的第三段，並將+號替代成空格。
 
+s1=('youtube.com/results?search_query=YOASOBI+-+Idol'.partition('=')) 
+print(s1[2].replace('+',' '))
 
-s1='https://www.youtube.com/results?search_query=YOASOBI+-+Idol'
-s2=(s1[45:])
-print(s2.replace('+',' '))
+s1=('www.youtube.com/results?search_query=I+want+it+that+way'.partition('='))
+print(s1[2].replace('+',' '))
