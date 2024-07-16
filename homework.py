a6891ba4e44ee63fec0ac8ed27905724da0c9d3d
Q <qwe246858@gmail.com>
2024-07-16 16:35:59 +0800

    716

---
+++ homework.py
@@ -0,0 +1,8 @@
+s1='https://www.youtube.com/results?search_query=LiSA-ADAMAS'
+s2=(s1[45:])
+print(s2.replace('+',' '))
+
+
+s1='https://www.youtube.com/results?search_query=YOASOBI+-+Idol'
+s2=(s1[45:])
+print(s2.replace('+',' '))
