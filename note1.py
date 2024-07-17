dfe5f128c2489997a550be9894516f0f92cef27d
Q <qwe246858@gmail.com>
2024-07-17 17:17:47 +0800

    feat:前五堂課程之筆記

--- note1.py
+++
@@ -1,4 +0,0 @@
-#副檔名 py
-#上箭頭-複製 cls-清除
-#執行程式python 空格 檔名 
-print("Hello Python 6")
---
+++ 簡介.py
@@ -0,0 +1,6 @@
+# 副檔名 .py
+# 複製上一句話 按鍵盤的上箭頭 
+# 清除 輸入cls
+# 執行程式 輸入 python 空格 檔名 
+print("Hello Python 6")
+# 在terminal輸入 pythin 簡介.py 即可得出Hello Python 6
